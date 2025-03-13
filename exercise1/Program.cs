dd5b609d8c4c491fc75728648ff1801ed649e8d0
ShantanuCog <shantanu.awasthi@cognizant.com>
2025-03-13 14:28:01 +0000

    exercise1 added functionality to meet specs

--- exercise1/Program.cs
+++ exercise1/Program.cs
@@ -1,17 +1,33 @@
-﻿// namespace: defining the context, container for classes
+﻿/*
+Functional Spec: Student class will take in the Grade arguments of english and maths grade
+Student class will be able to calculate their avg grade from their given maths/english grades
+Caveat: We cannot put calculateAvgGrade() in student class (Must follow SRP)
+*/
+
+// namespace: defining the context, container for classes
 namespace school {
 
     // Classes: Students, Grades, Teachers, Classes
   
     // obtain and set properties of the students
   class Student {
-    private string firstName     // private field (inaccesible)
+    private string firstName;    // private field (inaccesible)
     public string FirstName {    // property (pathway to accessibility)
       get { return firstName; }  // get the firstName
       set { firstName = value; } // set the firstName to 'value'
     }
     public string lastName;
     public int age;
+    // Add grades to student
+    public Grades grades;
+
+    // constructor to initialise the student with grades and age
+    public Student(string firstName, string lastName, int age, Grades grades) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.age = age;
+      this.grades = grades;
+    }
 
     // Methods: Introduce() -> "Hello, my name is 'firstName, 'lastName'"
     //          StateAge() -> "I am 'age' years old"
@@ -21,14 +37,14 @@ namespace school {
   class Grades {
     
     // obtain and set maths grades of students
-    private double mathsGrade
+    private double mathsGrade;
     public double MathsGrade {
       get { return mathsGrade; }  // get the mathsGrade
       set { mathsGrade = value; } // set the mathsGrade to 'value'
     }
 
     // obtain and set english grades of students
-    private double englishGrade
+    private double englishGrade;
     public double EnglishGrade {
       get { return englishGrade; }  // get the englishGrade
       set { englishGrade = value; } // set the englishGrade to 'value'
@@ -42,24 +58,22 @@ namespace school {
     
     // Methods: calculateAvgGrade() -> return average of english & maths grade
     public calculateAvgGrade() {
-      int avgGrade = (mathsGrade+englishGrade)/2;
+      int avgGrade = (mathsGrade + englishGrade) / 2;
       return avgGrade;
     }
   }
-
-    /*
-    Functional Spec: Student will take in the Grade arguments of english and maths grade
-    Student will be able to calculate their avg grade from their given maths/english grades
-    Caveat: We cannot put calculateAvgGrade() in student class (Must follow SRP)
-    */
 
   Grades grades1 = new Grades(45.5, 67);
   
   Student student1 = new Student(); // instantiate an object
   
   student1.FirstName = "John"; // set student1's firstName to "John"
+  student1.LastName = "Doe";   // set student1's lastName to "Doe"
+  student1.Age = 18;           // set student1's age to 18
 
-  Console.log(Student1.FirstName);
+//   Console.log(Student1.FirstName);
+// display the average grade of student1
+  Console.log(student1.grades.calculateAvgGrade());
   
 }
 
